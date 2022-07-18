9b214a85ac1b95a0789f7f2be34fd306b783b3f9
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:11:20 +0530

    toastfy setup done in redux.js

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,10 +1,15 @@
 import react from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import store from './store';
 import App from '../App';
 import { Provider } from 'react-redux';
 
 function Redux() {
   return;
-  <Provider store={store}>Redux</Provider>;
+  <Provider store={store}>
+    <App />
+    <ToastContainer />
+  </Provider>;
 }
 export default Redux;
