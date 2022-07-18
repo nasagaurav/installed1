1fdc0be8811d857e55599f7411a6a06d1f40e740
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:45:36 +0530

    ux setup in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
 
 import App from './App';
 
@@ -8,6 +11,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>
   </StrictMode>
 );
