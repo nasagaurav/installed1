c434e18d741735f579dd20af6cc04c96067b656b
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:49:18 +0530

    made index.js file in source_src_folder.

---
+++ src/source/index.js
@@ -0,0 +1,64 @@
+
+
+
+export const getAllProducts = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/products.json')
+    .then((res) => res.data)
+    .then((d) => d.map((x) => ({ ...x, image: img(x.id) })))
+    .catch((e) => []);
+
+  return data;
+};
+export const getAllTags = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/tags.json')
+    .then((res) => res.data)
+    .catch((e) => []);
+
+  return data;
+};
+export const getAllFilters = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/filters.json')
+    .then((res) => res.data)
+    .catch((e) => []);
+
+  return data;
+};
+export const getAllProducts = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/products.json')
+    .then((res) => res.data)
+    .then((d) => d.map((x) => ({ ...x, image: img(x.id) })))
+    .catch((e) => []);
+
+  return data;
+};
+export const getAllTags = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/tags.json')
+    .then((res) => res.data)
+    .catch((e) => []);
+
+  return data;
+};
+export const getAllFilters = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/filters.json')
+    .then((res) => res.data)
+    .catch((e) => []);
+
+  return data;
+};
+export const getAllUsers = async () => {
+  let data = await axios
+    .get('https://l-ecommerce-default-rtdb.firebaseio.com/users.json')
+    .then((res) => res.data)
+    .then((d) => {
+      transform(d);
+    })
+    .catch((e) => []);
+
+  return data;
+};
