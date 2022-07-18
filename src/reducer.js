a77971f439a0654f118cb8b5c17fa9f9693548f9
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:50:57 +0530

    reducer user initial state added

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,24 @@
-function reducer() {}
+
+const initialState = {
+  users :[];
+  bookings:[];
+  flights:[];
+  role:"",
+  loggedin:false,
+  user:null,
+
+
+
+
+}
+
+
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
 
 export default reducer;
