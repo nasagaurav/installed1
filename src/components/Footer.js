885a79423290efeb23a5a743b3670d6931a2f5b3
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:26:54 +0530

    footer modified

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+import react from 'react';
+
+function Footer() {
+  return <div>Footer</div>;
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+
+function Header()
+{
+	return <div> 
+	Header
+	   </div>
+
+  }
+ export default Header 
