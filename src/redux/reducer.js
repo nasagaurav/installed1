e04cfdc05d2f6c9023cb85252bea00df94cc931d
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:17:18 +0530

    action switch case enables in reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,18 @@
-const initialState = {};
+const initialState = {
+  filters:[],
+  tags:[],
+  Products:[],
+  }};
 
 function reducer(state = initialState, action) {
   console.log('reducer', action);
   switch (action.type) {
-    default:
+    case 'getAllProducts':
+      return { ...state, products: action.payload };
+    case 'getAllTags':
+      return { ...state, tags: action.payload };
+    case 'getAllFilter':
+      return { ...state, filter: action.payload };
       return state;
   }
 }
