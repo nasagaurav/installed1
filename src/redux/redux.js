bc1965690548064d0946a5ae6e2b0655276e5985
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:15:31 +0530

    redux-added

---
+++ src/redux/redux.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import store from './store';
+import App from '../App';
+import { Provider } from 'react-redux';
+
+function Redux() {
+  return;
+  <Provider store={store}>Redux</Provider>;
+}
+export default Redux;
