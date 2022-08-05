c5077244e28faefdef33a6a80bad9408163eabfd
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 15:04:46 +0530

    1 file modified

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Signup() {
-  return <div>Signup</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="name" {...register('name')} />
+        {errors.name && <p>name required</p>}
+
+        <input defaultValue="email" {...register('email')} />
+        {errors.email && <p>email required</p>}
+        <input defaultValue="phone" {...register('phone')} />
+        {errors.phone && <p>phone required</p>}
+        <input defaultValue="password" {...register('password')} />
+        {errors.password && <p>password required</p>}
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 export default Signup;
