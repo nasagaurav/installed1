579996ab163a9233f0ba26d50caeb57a65cd4fda
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 14:56:25 +0530

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { removeStorage } from '../services';
 
 function Header() {
   const state = useSelector((s) => s);
-
+  const state = useDispatch();
   const { user, loggedin } = state;
+  const logout = () => {
+    removeStorage('uid');
+    removeStorage('name');
+    removeStorage('email');
+    removeStorage('phone');
+    removeStorage('password');
+    dispatch ({type:'logout'})
+  };
+
   return (
     <header>
       <div className="logo">Ecommerce</div>
-
       <div>
         <Link to="/">Home</Link>
 
@@ -18,7 +27,7 @@ function Header() {
         {loggedin && <Link to="Profile">Profile</Link>}
         {loggedin && <Link to="Order">Order</Link>}
         {loggedin && <Link to="Cart">Cart</Link>}
-        {loggedin && <Link to="Logout">Logout{user.name}</Link>}
+        {loggedin && <button onClick={Logout}>Logout({user.name})</button>}
       </div>
     </header>
   );
